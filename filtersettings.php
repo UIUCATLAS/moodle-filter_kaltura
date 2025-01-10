97c366820bb4fd7482387e29f03063bdaf40e143
Liam Moran <moran@illinois.edu>
2025-01-10 12:41:57 -0600

    added default field value

--- filtersettings.php
+++ filtersettings.php
@@ -32,7 +32,8 @@ if ($ADMIN->fulltree) {
     // this configuration setting is stored in the config_plugins table instead
     $settings->add(new admin_setting_configtext('filter_kaltura/preferred_playerid',
         get_string('preferred_playerid', 'filter_kaltura'),
-        get_string('preferred_playerid_help', 'filter_kaltura')
+        get_string('preferred_playerid_help', 'filter_kaltura',
+        '')
     ));
 
     $settings->add(new admin_setting_configtextarea('filter_kaltura_uris',
