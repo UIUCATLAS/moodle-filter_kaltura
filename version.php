f5d1e13180104e52dbba23bd2575805d0a340f0a
Liam Moran <moran@illinois.edu>
2025-09-05 10:45:01 -0500

    ATLAS-387: Allow site-admin default Kaltura player

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024100702; //version date YYYYMMDDXX 10 represent 3.0 for future option to moodle use 2 digit version
+$plugin->version = 2025090500; //version date YYYYMMDDXX 10 represent 3.0 for future option to moodle use 2 digit version
 $plugin->component  = 'filter_kaltura';
 $plugin->release = 'Kaltura release 4.5.1';
 $plugin->requires = 2024042200;
